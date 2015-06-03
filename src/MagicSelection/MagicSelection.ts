7bab549ab42c015ba162e1c868ffbe70e04c4dd9
Merwin van Dijk <merwinvandijk@gmail.com>
2015-06-03 19:04:00 +0200

    test/debug pagina

---
+++ src/MagicSelection/MagicSelection.ts
@@ -0,0 +1,149 @@
+
+class Point {
+	x : number;
+	y : number;
+
+	constructor(x : number, y : number) {
+		this.x = x;
+		this.y = y;
+	}
+}
+
+class MagicSelection {
+	imageData : ImageData;
+	magicWandColor : number[];
+	bitMaskData : number[];
+	treshold : number;
+
+	constructor() {
+		this.imageData = null;
+		this.magicWandColor = null;
+		this.bitMaskData = [];
+		this.treshold = 0.0;
+	}
+
+	/* Given image data, pixel and treshold value a bitmask for the magic wand is returned. 
+		Algorithm checks for lines horizontally and adds line elements above and below to the 
+		stack when they match the color of the given point. */
+	getSelection(imageData : ImageData, x : number, y : number, treshold : number) {
+		var stack = [];
+		var curLine = [];
+		var newLine = [];
+
+		this.imageData = imageData;
+
+		/* Fill bitmask with zeros. */
+		for (var i = 0; i < imageData.data.length / 4; i++) {
+			this.bitMaskData.push(0);
+		}
+
+		this.magicWandColor = this.getColorPoint(x, y);
+		// console.log(x);
+		// console.log(y);
+		// console.log(this.magicWandColor);
+		// console.log(imageData.data[4*(43*imageData.width+100)]);
+
+		/* Use searchLine to get a horizontal line of points with colors next to given point. */
+		curLine = this.searchLine(x, y);
+		console.log(curLine);
+		stack.push(curLine);
+		/* Keep popping line elements from stack until stack is empty. */
+		while (stack.length > 0 && curLine != null) {
+			curLine = stack.pop();
+			// console.log(curLine);
+			// console.log(curLine.length);
+			// console.log(stack.length);
+
+
+
+			for (var i = 0; i < curLine.length; i++) {
+				this.bitMaskData[curLine[i].x + curLine[i].y * imageData.width] = 255;
+
+				/* Check if there is a line element above current position. */
+				newLine = this.searchLine(x, y - 1);
+				if (newLine != null) {
+					stack.push(newLine);
+				}
+
+				/* Check if there is a line element below current position. */
+				newLine = this.searchLine(x, y + 1);
+				if (newLine != null) {
+					stack.push(newLine);
+				}				
+			}
+		}
+
+		return this.bitMaskData;
+	}
+
+	/* Return array of points on same line as given point. */
+	searchLine(startX : number, startY : number) {
+		var line = [];
+		var left : number = startX;
+		var right : number = startY;
+	
+		/* Check if a valid position is given. */
+		if (startY < 0 || startY >= this.imageData.height || startX < 0 || startX >= this.imageData.width ) {
+			return null;
+		}
+
+		/* Check if point is marked in bitMaskData as part of the selection. */ 
+		if (this.bitMaskData[startX + startY * this.imageData.width] != 0) {
+			return null;
+		}
+
+		/* Check if point matches magicWandColor. */
+		if (this.matchPoint(startX, startY) == false) {
+			return null;
+		}
+
+		/* Search left from starting point. */
+		for (var x = startX - 1; x >= 0; x--) {
+			if (this.matchPoint(x, startY) == false) {
+				left = x;
+				break;
+			}	
+		}
+
+		/* Search right from starting point. */
+		for (var x = startX + 1; x < this.imageData.width; x++) {
+			if (this.matchPoint(x, startY) == false) {
+				right = x;
+				break;
+			}	
+		}
+
+		/* Make line by adding all ... */
+		for (var x = left; x <= right; x++) {
+			line.push(new Point(x, startY));
+		}
+
+		return line;
+	}
+
+	getColorPoint(x : number, y : number) {
+		var Red : number	= this.imageData.data[(x + y * this.imageData.width) * 4];
+		var Green : number	= this.imageData.data[(x + y * this.imageData.width) * 4 + 1];
+		var Blue  : number	= this.imageData.data[(x + y * this.imageData.width) * 4 + 2];
+		var Alpha : number	= this.imageData.data[(x + y * this.imageData.width) * 4 + 3];
+
+		return [Red, Green, Blue, Alpha];
+	}
+
+	matchPoint(x : number, y : number) {
+		var curColor = this.getColorPoint(x, y);
+		var value = 0.0;
+
+		for (var i = 0; i < 4; i++) {
+			value += Math.abs(this.magicWandColor[i] - curColor[i]);
+		}
+
+		value /= 4.0 * 255.0; 
+
+		if (value <= this.treshold) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
--- src/app/content/content.logic.js
+++ src/app/content/content.logic.js
@@ -1,3 +1,77 @@
 angular.module('ariana').controller('contentCtrl', function($scope) {
-    $scope.somevar = 'Data-binding';
+    setCanvasSize();
+    var image = new Image();
+    image.src = "assets/image.png";
+
+    var canvas = document.getElementById('main-canvas');
+    var context = canvas.getContext('2d');
+
+	image.onload = function() {
+		context.drawImage(image, 50, 50);
+		var imageData = context.getImageData(50, 50, 201, 201);
+
+		var magic = new MagicSelection();
+		var bitmask = magic.getSelection(imageData, 43, 100, 0.125);
+
+		console.log(bitmask.length);
+		console.log(bitmask);
+		console.log(imageData.data.length);
+
+
+		for (var i=0; i < bitmask.length; i++) {
+			if (bitmask[i] != 0) {
+				imageData.data[4*i] = 0;
+				imageData.data[4*i+1] = 0;
+				imageData.data[4*i+2] = 0;
+				imageData.data[4*i+3] = 0;
+				console.log("bitmask ongelijk aan 0");
+			} else {
+				imageData.data[4*i] = 255;
+				imageData.data[4*i+1] = 255;
+				imageData.data[4*i+2] = 255;
+				imageData.data[4*i+3] = 255;
+			}
+		}
+
+		context.putImageData(imageData,400,50);
+		console.log(magic);
+		console.log(image);
+	};
+
+	// for (var i = 0; i < imageData.data.length; i++) {
+	// 	imageData.data[i] = 255;
+	// }
+
+	// console.log(imageData);
+	// context.putImageData(imageData, 400, 100);
+
+
+	// console.log(image);
+	// var imageObj = new Image();
+	// imageObj.onload = function() {
+	// 	context.drawImage(imageObj, 69, 50);
+	//};
+	//imageObj.src = 'http://www.html5canvastutorials.com/demos/assets/darth-vader.jpg';
+    // context.drawImage(image, 100, 100);
+    // console.log(image);
+    // TODO set resize canvas on resize
+    
+    // TODO set resize canvas on orientation change 
+    
 });
+
+/* This function sets the canvas full screen. It uses the fact that hdpi 
+screen require a larger canvas size. */
+function setCanvasSize() {
+    var canvas = document.getElementById('main-canvas');
+    var desiredWidthInCSSPixels  = window.innerWidth;
+    var desiredHeightInCSSPixels = window.innerHeight;
+    
+    canvas.style.width  = desiredWidthInCSSPixels  + "px";
+    canvas.style.height = desiredHeightInCSSPixels + "px";
+    
+    var devicePixelRatio = window.devicePixelRatio || 1;
+    
+    canvas.width  = desiredWidthInCSSPixels  * devicePixelRatio;
+    canvas.height = desiredHeightInCSSPixels * devicePixelRatio;  
+}
