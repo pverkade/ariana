1744005de47850429d60b6ed56eefb6eac085a6b
Peter Verkade <peter.m.verkade@gmail.com>
2015-06-03 15:13:38 +0200

    toolbox

--- src/app/toolbox/toolbox.logic.js
+++ src/app/toolbox/toolbox.logic.js
@@ -1,8 +1,8 @@
 angular.module('ariana').controller('toolboxCtrl', function($scope) {
     
-    //$scope.leftColor  = "#ffaa00";
-    //$scope.rightColor = "#88cc44";
-    //updateColorPreview();
+    $scope.leftColor  = "#ffaa00";
+    $scope.rightColor = "#88cc44";
+    updateColorPreview();
     
     /* make tools. */
     var toolset = document.getElementById('toolset');   
@@ -85,56 +85,118 @@ angular.module('ariana').controller('toolboxCtrl', function($scope) {
             };
             request.send();
         }
-    }  
-});
-
-/* This function shows a certain toolblock. */
-function showSubToolset(setnumber) {
-
-    /* Set all other category-selection tools as disabled. */
-    var toolset = document.getElementById('toolset').children; 
-    for (var i = 0; i < toolset.length; i++) {
-        if (i == setnumber) toolset[i].toolEnabled = true;
-        else toolset[i].toolEnabled = false;
-    } 
-    
-    /* Move the desired toolblock in view. Move all tool blocks to their 
-     * original position. */
-    var subtoolsets = document.getElementById('sub-toolsets').children;  
-    for (var i = 0; i < subtoolsets.length; i++) {
-        if (i == setnumber) subtoolsets[i].style.transform = "translate(144px, 0px)";
-        else subtoolsets[i].style.transform = "translate(-144px, 0px)";
     }
-}
-
-/* This function hides all toolsets. */
-function hideSubToolsets(){
-
-    /* Set all category-selection tools as disabled. */
-    var toolset = document.getElementById('toolset').children; 
-    for (var i = 0; i < toolset.length; i++) {
-        toolset[i].toolEnabled = false;
-    } 
-
-    /* Move all tool blocks to their original position. */
-    var subtoolsets = document.getElementById('sub-toolsets').children;  
-    for (var i = 0; i < subtoolsets.length; i++) {
-        subtoolsets[i].style.transform = "translate(-144px, 0px)";
-    }
-}
-/*
-function setLeftColor(color){
-    $scope.leftColor = color;
-    updateColorPreview();
-}
-
-function setRightColor(color){
-    $scope.rightColor = color;
-    updateColorPreview(); 
-}
-
-function updateColorPreview(leftColor, rightColor){
-    document.getElementById('color-left').style.background  = $scope.leftColor; 
-    document.getElementById('color-right').style.background = $scope.rightColor; 
-} */
     
+    document.getElementById('color-preview').onclick = switchColors();
+    
+    function switchColors(){
+        console.log("switch");
+        var t = $scope.leftColor;
+        $scope.leftColor = $scope.rightColor;
+        $scope.rightColor = t;
+        updateColorPreview();
+        console.log("done");
+    }
+    
+    function setLeftColor(color){
+        $scope.leftColor = color;
+        updateColorPreview();
+    }
+
+    function setRightColor(color){
+        $scope.rightColor = color;
+        updateColorPreview(); 
+    }
+
+    function updateColorPreview(leftColor, rightColor){
+        document.getElementById('color-left').style.background  = $scope.leftColor; 
+        document.getElementById('color-right').style.background = $scope.rightColor; 
+    }   
+    
+   /* This function shows a certain toolblock. */
+    function showSubToolset(setnumber) {
+
+        /* Set all other category-selection tools as disabled. */
+        var toolset = document.getElementById('toolset').children; 
+        for (var i = 0; i < toolset.length; i++) {
+            if (i == setnumber) toolset[i].toolEnabled = true;
+            else toolset[i].toolEnabled = false;
+        } 
+        
+        /* Move the desired toolblock in view. Move all tool blocks to their 
+         * original position. */
+        var subtoolsets = document.getElementById('sub-toolsets').children;  
+        for (var i = 0; i < subtoolsets.length; i++) {
+            if (i == setnumber) subtoolsets[i].style.transform = "translate(144px, 0px)";
+            else subtoolsets[i].style.transform = "translate(-144px, 0px)";
+        }
+        
+        return;
+    }
+
+    /* This function hides all toolsets. */
+    function hideSubToolsets(){
+
+        /* Set all category-selection tools as disabled. */
+        var toolset = document.getElementById('toolset').children; 
+        for (var i = 0; i < toolset.length; i++) {
+            toolset[i].toolEnabled = false;
+        } 
+
+        /* Move all tool blocks to their original position. */
+        var subtoolsets = document.getElementById('sub-toolsets').children;  
+        for (var i = 0; i < subtoolsets.length; i++) {
+            subtoolsets[i].style.transform = "translate(-144px, 0px)";
+        }
+        
+        return;
+    }
+}   
+    
+    
+    
+    
+    
+angular.module('ariana').directive('color-preview', ['$document', function($document) {
+return {
+    link: function(scope, element, attr) {
+      var startX = 0, startY = 0, x = 0, y = 0;
+
+      element.css({
+            /*
+           position:        'relative',
+           border:          '1px solid red',
+           backgroundColor: 'lightgrey',
+           cursor:          'pointer'
+            */
+      });
+
+      element.on('mousedown', function(event) {t
+            /*event.preventDefault();
+            startX = event.pageX - x;
+            startY = event.pageY - y;
+            $document.on('mousemove', mousemove);
+            $document.on('mouseup', mouseup);
+            */
+            switchColors();
+      });
+
+      function mousemove(event) {
+            y = event.pageY - startY;
+            x = event.pageX - startX;
+            element.css({
+              top: y + 'px',
+              left:  x + 'px'
+            });
+      }
+
+      function mouseup() {
+            $document.off('mousemove', mousemove);
+            $document.off('mouseup', mouseup);
+      }
+    }
+  };
+}]);
+
+
+
