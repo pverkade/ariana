5737ddd67eca133a8c3421246b03dcc6d04d9fca
Tristan van Vaalen <tvvaalen@gmail.com>
2015-06-04 16:47:27 +0200

    Angular interface

--- src/app/content/content.logic.js
+++ src/app/content/content.logic.js
@@ -3,8 +3,17 @@ angular.module('ariana').controller('contentCtrl', function($scope) {
     
     // TODO set resize canvas on resize
     
-    // TODO set resize canvas on orientation change 
-    
+    // TODO set resize canvas on orientation change
+
+    $scope.mouseMove = function(e) {
+        $scope.config.mouse.location.x = e.x;
+        $scope.config.mouse.location.y = e.y;
+    }
+
+    $scope.mouseClick = function(e) {
+        $scope.config.mouse.click.x = e.x;
+        $scope.config.mouse.click.y = e.y;
+    }
 });
 
 /* This function sets the canvas full screen. It uses the fact that hdpi 
