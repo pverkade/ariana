2a86d620a74a0db818f08ceaa33fb08570419968
Peter Verkade <peter.m.verkade@gmail.com>
2015-06-05 11:07:46 +0200

    dirty fix for SVG color from StackOverflow

--- src/app/toolbox/toolbox.logic.js
+++ src/app/toolbox/toolbox.logic.js
@@ -2,6 +2,36 @@
  * The toolset controller contains all logic of the toolsets
  */
 angular.module('ariana').controller('toolsetCtrl', function($scope) {
+    
+    // SVG FIX FOT STACK OVEFLOW
+    $('.svg-img').each(function(){
+        var $img = $(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        $.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = $(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+    });
+    
     $scope.collapsed = true;
 
     /*
