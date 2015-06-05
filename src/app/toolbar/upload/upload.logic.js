d47e6de290d31f6ac385f51d3f7cf9435b142f0f
Jurre Wolsink <jurre.wolsink@gmail.com>
2015-06-05 17:25:47 +0200

    created import functionality

--- src/app/toolbar/upload/upload.logic.js
+++ src/app/toolbar/upload/upload.logic.js
@@ -1,9 +1,13 @@
-app.controller('UploadModalCtrl', ['$scope', '$modalInstance', 
-    function ($scope, $modalInstance) {
-        console.log('in UploadModalCtrl');
-
+app.controller('UploadModalCtrl', ['$scope', '$modalInstance', 'Upload',
+    function ($scope, $modalInstance, Upload) {
         $scope.close = function () {
             $modalInstance.dismiss();
+        };
+ 
+        $scope.upload = function () {
+            $scope.files.forEach(function (file) {
+                console.log(file);
+            })
         };
     }
 ])
