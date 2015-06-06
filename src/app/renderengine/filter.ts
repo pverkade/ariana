4385f437fbe477a93f6ca48641c9e077fff13b47
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2015-06-06 10:03:14 +0200

    Fix build error

--- src/app/renderengine/filter.ts
+++ src/app/renderengine/filter.ts
@@ -12,7 +12,7 @@ function normalize100(x, low, high) {
     return clamp(x, 0, 100) / 100 * (high - low) + low;
 }
 
-enum FilterType {Brightness};
+enum FilterType {Brightness, Contrast};
 enum FilterValueType {Slider};
 
 class FilterProgram extends BaseProgram {
