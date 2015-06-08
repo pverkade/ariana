dd04cc02c15f9c2e472e00cc3077a0583df2f70c
Mathijs Molenaar <mathijs.l.molenaar@gmail.com>
2015-06-08 12:35:52 +0200

    Y is upside down

--- src/app/renderengine/render-engine.ts
+++ src/app/renderengine/render-engine.ts
@@ -130,7 +130,7 @@ class RenderEngine {
 
     getPixelColor(x : number, y : number) : Uint8Array {
         var value = new Uint8Array(4);
-        this.gl.readPixels(x, y, 1, 1, this.gl.RGBA, this.gl.UNSIGNED_BYTE, value);
+        this.gl.readPixels(x, this.height-y, 1, 1, this.gl.RGBA, this.gl.UNSIGNED_BYTE, value);
         console.log(value[2]);
         return value;
     }
