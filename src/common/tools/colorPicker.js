392a59b05242be9adaa55ff126ffaad6e1fc6d7a
Peter Verkade <peter.m.verkade@gmail.com>
2015-06-09 09:14:29 +0200

    fixed cursor on background

--- src/common/tools/colorPicker.js
+++ src/common/tools/colorPicker.js
@@ -1,25 +1,28 @@
 var colorPicker = {
     
     start: function() {
-        $("#main-canvas").css("cursor", "crosshair");
+        $("#background").css("cursor", "crosshair");
     },
     
     mouseDown: function($scope) {
-        var x = $scope.config.mouse.current.x - canvasLocationX;
-        var y = $scope.config.mouse.current.y - canvasLocationY;
-        
-        var value = $scope.renderEngine.getPixelColor(x, y);
-        
-        /* Write color to config. */
-        $scope.config.tools.colors.primary.r = value[0];
-        $scope.config.tools.colors.primary.g = value[1];
-        $scope.config.tools.colors.primary.b = value[2];
+        //
     },
     
     mouseUp: function($scope) {
     },
     
-    mouseMove: function($scope) {    
-        // TODO call mouseDown
+    mouseMove: function($scope) {
+        /* Get mouse location on canvas. */
+        // TODO also use zoom-factor etc.
+        var x = $scope.config.mouse.current.x - canvasLocationX;
+        var y = $scope.config.mouse.current.y - canvasLocationY;
+        
+        /* Get the color of that pixel. */
+        var color = $scope.renderEngine.getPixelColor(x, y);
+        
+        /* Write color to config. */
+        $scope.config.tools.colors.primary.r = color[0];
+        $scope.config.tools.colors.primary.g = color[1];
+        $scope.config.tools.colors.primary.b = color[2];
     },
 }
