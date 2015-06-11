5488208d19a5ac23f0ed721840736b709629fd62
Merwin van Dijk <merwinvandijk@gmail.com>
2015-06-11 23:14:36 +0200

    begin LooseSelection klasse

--- src/app/renderengine/LooseSelection.ts
+++ src/app/renderengine/LooseSelection.ts
@@ -1,3 +1,13 @@
+class LineElt {
+	point1: Point;
+	point2: Point;
+
+	constructor(point1: Point, point2: Point) {
+		this.point1 = point1;
+		this.point2 = point2;
+	}
+}
+
 class LooseSelection {
 	points : Point[];
 
@@ -8,7 +18,45 @@ class LooseSelection {
 	addPoint(point : Point) {
 		if (this.points[this.points.length-1].x != point.x &&
 			this.points[this.points.length-1].y != point.y) {
+			
 			this.points.push(point);
 		}
+	}
+
+	checkIntersection(line1 : LineElt, line2 : LineElt) {
+
+	}
+
+	getIntersection(line1 : LineElt, line2 : LineElt) : Point{
+		var intersectionPoint : Point;
+
+		// ..
+
+		return intersectionPoint;
+	}
+
+	getkBoundingPath() {
+		var intersectionPoint : Point;
+		var newLineElt : LineElt;
+		var prevLineElt : LineElt;
+		var point = this.points[this.points.length - 1];
+
+		if (this.points.length >= 1) {
+			newLineElt = new LineElt(this.points[this.points.length -1], point);
+
+			for (var i = this.points.length - 2; i >= 0; i--) {
+				prevLineElt = new LineElt(this.points[i], this.points[i+1]);
+
+				if (this.checkIntersection(prevLineElt, newLineElt)) {
+					// intersectionPoint = this.getIntersection(prevLineElt, newLineElt);
+					this.points[i].x = intersectionPoint.x;
+					this.points[i].y = intersectionPoint.y;
+
+					return this.points.slice(i, this.points.length);
+				}
+			}
+		}
+
+	return [];
 	}
 }
