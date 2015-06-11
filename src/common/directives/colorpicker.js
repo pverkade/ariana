0093286b89d5f52486aa3a79fa982ffdbaf111a6
Matthijs de Wit <matthijs111@gmail.com>
2015-06-11 15:31:12 +0200

    colorpickerctrl added

--- src/common/directives/colorpicker.js
+++ src/common/directives/colorpicker.js
@@ -5,59 +5,8 @@ angular.module('ariana').directive('colorpicker', function() {
         restrict: 'EA',
         
         // HTML template
-        template: "<canvas class='palette' width='256px' height='256px'> 
-        </canvas>
-        <canvas class='hue' width='32px' height='256px'>
-        </canvas>
-        <div class='preview'>
-        </div>
-        <div class='textinput'>
-            R: 
-            <input type='number' class='rgbhsv' name='R' 
-            onchange='update_val(event)' min='0' max='255'>
-        </div>
-        <div class='textinput'>
-            G: 
-            <input type='number' class='rgbhsv' name='G' 
-            onchange='update_val(event)' min='0' max='255'>
-        </div>
-        <div class='textinput'>
-            B: 
-            <input type='number' class='rgbhsv' name='B' 
-            onchange='update_val(event)' min='0' max='255'>
-        </div>
-        <div class='textinput'>
-            H: 
-            <input type='number' class='rgbhsv' name='H' 
-            onchange='update_val(event)' min='0' max='360'>
-        </div>
-        <div class='textinput'>
-            S: 
-            <input type='number' class='rgbhsv' name='S' 
-            onchange='update_val(event)' min='0' max='100'>
-        </div>
-        <div class='textinput'>
-            V: 
-            <input type='number' class='rgbhsv' name='V' 
-            onchange='update_val(event)' min='0' max='100'>
-        </div>
-        <div class='textinput'>
-            Hex: 
-            <input type='text' class='hex' onchange='update_hex(event)'>
-        </div>",
-        
-        // dont overwrite <colorpicker> element with HTML
-        replace: false,
-    
-        /*
-        controller: ["$scope", function($scope) {
-            console.log("contoller");
-        }],
-        */
-    
-        /*
-        link: function($scope, $element, $attibutes) {
-        }, 
-        */
+        template: '<canvas class="palette" width="256px" height="256px"></canvas><canvas class="hue" width="32px" height="256px"></canvas><div class="preview"></div><div class="textinput">R: <input type="number" class="rgbhsv" name="R" onchange="update_val(event)" min="0" max="255"></div><div class="textinput">G: <input type="number" class="rgbhsv" name="G" onchange="update_val(event)" min="0" max="255"></div><div class="textinput">B: <input type="number" class="rgbhsv" name="B" onchange="update_val(event)" min="0" max="255"></div><div class="textinput">H: <input type="number" class="rgbhsv" name="H" onchange="update_val(event)" min="0" max="360"></div><div class="textinput">S: <input type="number" class="rgbhsv" name="S" onchange="update_val(event)" min="0" max="100"></div><div class="textinput">V: <input type="number" class="rgbhsv" name="V" onchange="update_val(event)" min="0" max="100"></div><div class="textinput">Hex: <input type="text" class="hex" onchange="update_hex(event)"></div>',
+
+        })
     };
 });
