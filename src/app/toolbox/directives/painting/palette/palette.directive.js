6586b93c08a9a4549463c60437286ccf5fcac5d0
Tristan van Vaalen <tvvaalen@gmail.com>
2015-06-12 16:46:09 +0200

    Added default toolfunctions to all tools.

--- src/app/toolbox/directives/painting/palette/palette.directive.js
+++ src/app/toolbox/directives/painting/palette/palette.directive.js
@@ -10,4 +10,44 @@ angular.module('ariana').directive('palette', function() {
 angular.module('ariana').controller('PaletteCtrl', function($scope) {
 	$scope.toolname = 'palette'
 	$scope.active = $scope.config.tools.activeTool == $scope.toolname;
+
+	/* init */
+	$scope.init = function() {
+		$scope.setCursor('default');
+	};
+
+	/* onMouseDown */
+	$scope.mouseDown = function() {
+
+	};
+
+	/* onMouseUp */
+	$scope.mouseUp = function() {
+
+	};
+
+	/* onMouseMove */
+	$scope.mouseMove = function() {
+		
+	};
+	/*
+	 * This will watch for this tools' "active" variable changes.
+	 * When "active" changes to "true", this tools functions need to
+	 * be registered to the global config.
+	 * This functions NEEDS to be in each tools controller for
+	 * the tool to function. Please assign the correct toolfunctions
+	 * to the "activeToolFunctions" object.
+	 * Always call "init" first;
+	 */
+	$scope.$watch('active', function(nval, oval) {
+		if (nval) {
+			$scope.init();
+
+			$scope.config.tools.activeToolFunctions = {
+				mouseDown: $scope.mouseDown,
+				mouseUp: $scope.mouseUp,
+				mouseMove: $scope.mouseMove
+			};
+		}
+	}, true);
 });
