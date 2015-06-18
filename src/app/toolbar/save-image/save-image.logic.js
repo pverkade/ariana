dcd970855f5eb1b681e1652ebd664583a17e3ae5
David Veenstra <davidjulianveenstra@gmail.com>
2015-06-18 17:34:57 +0200

    added save image modal

---
+++ src/app/toolbar/save-image/save-image.logic.js
@@ -0,0 +1,56 @@
+/**
+ * Created by zeta on 6/18/15.
+ */
+
+app.controller('SaveImageModalController', ['$scope', '$modalInstance', '$modal',  function($scope, $modalInstance, $modal) {
+    $scope.format = "png";
+    $scope.quality = 90;
+    $scope.filename = "untitled";
+
+    $scope.closeSaveImageModal = function () {
+        $modalInstance.dismiss();
+    };
+
+    $scope.saveImage = function () {
+        console.log("quality", $scope.quality);
+        console.log("format", $scope.format);
+        console.log("filename", $scope.filename);
+    };
+
+    /* This functions saves the canvas to an image-file. */
+    $scope.saveImage = function() {
+        var toolFunctions = $scope.config.tools.activeToolFunctions;
+
+        if (toolFunctions && toolFunctions.stop) {
+            toolFunctions.stop();
+        }
+
+        /* Receive image data in base64 encoding. */
+        var image = $scope.renderEngine.renderToImg();
+        var data = image.substr(image.indexOf(',') + 1).toString();
+        var url = "/save-image";
+
+        var dataInput = document.createElement("input") ;
+        dataInput.setAttribute("name", 'image-data') ;
+        dataInput.setAttribute("value", data);
+        dataInput.setAttribute("type", "hidden");
+
+        var nameInput = document.createElement("input") ;
+        nameInput.setAttribute("name", 'filename') ;
+        nameInput.setAttribute("value", $scope.filename);
+
+        var formatInput = document.createElement("input") ;
+        formatInput.setAttribute("name", 'format') ;
+        formatInput.setAttribute("value", $scope.format);
+
+        var myForm = document.createElement("form");
+        myForm.method = 'post';
+        myForm.action = url;
+        myForm.appendChild(dataInput);
+        myForm.appendChild(nameInput);
+
+        document.body.appendChild(myForm) ;
+        myForm.submit() ;
+        document.body.removeChild(myForm) ;
+    };
+}]);
--- src/app/toolbar/toolbar.logic.js
+++ src/app/toolbar/toolbar.logic.js
@@ -14,39 +14,6 @@ app.controller('ToolbarController', ['$scope', '$modal',
          * hidden when the user is clicking on the canvas/background. */
         $scope.checkVisible = function() {
             return (!($scope.config.mouse.button[1] || $scope.config.mouse.button[2] || $scope.config.mouse.button[3]));
-        }
-      
-        /* This functions saves the canvas to an image-file. */
-        $scope.saveImage = function() {
-            var toolFunctions = $scope.config.tools.activeToolFunctions;
-
-            if (toolFunctions && toolFunctions.stop) {
-                toolFunctions.stop();
-            }
-
-            /* Receive image data in base64 encoding. */
-            var image = $scope.renderEngine.renderToImg();
-            var data = image.substr(image.indexOf(',') + 1).toString();
-            var url = "/save-image";
-
-            var dataInput = document.createElement("input") ;
-            dataInput.setAttribute("name", 'image-data') ;
-            dataInput.setAttribute("value", data);
-            dataInput.setAttribute("type", "hidden");
-
-            var nameInput = document.createElement("input") ;
-            nameInput.setAttribute("name", 'image-name') ;
-            nameInput.setAttribute("value", "hello_world" + '.png');
-
-            var myForm = document.createElement("form");
-            myForm.method = 'post';
-            myForm.action = url;
-            myForm.appendChild(dataInput);
-            myForm.appendChild(nameInput);
-
-            document.body.appendChild(myForm) ;
-            myForm.submit() ;
-            document.body.removeChild(myForm) ;
         };
         
         /* This function opens the newfile modal. */
@@ -77,6 +44,15 @@ app.controller('ToolbarController', ['$scope', '$modal',
                 scope: $scope,
                 size: 'lg'
             });
+        };
+
+        $scope.openSaveImageModal = function() {
+            var modalInstance = $modal.open({
+                templateUrl: 'app/toolbar/save-image/save-image.tpl.html',
+                controller: 'SaveImageModalController',
+                scope: $scope,
+                size: 'lg'
+            })
         };
         
         $scope.filter = {
