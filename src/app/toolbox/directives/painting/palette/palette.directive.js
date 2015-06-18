4941c65a3d1a48a93c448152cd3d7fbdf3091779
Matthijs de Wit <matthijs111@gmail.com>
2015-06-18 15:37:32 +0200

    now works on firefox, yay

--- src/app/toolbox/directives/painting/palette/palette.directive.js
+++ src/app/toolbox/directives/painting/palette/palette.directive.js
@@ -8,31 +8,27 @@ app.directive('palette', function() {
 });
 
 app.controller('PaletteCtrl', function($scope) {
-	$scope.toolname = 'palette'
-	$scope.active = $scope.config.tools.activeTool == $scope.toolname;
+    $scope.toolname = 'palette'
+    $scope.active = $scope.config.tools.activeTool == $scope.toolname;
 
-	hsv = RGBtoHSV(	$scope.config.tools.colors.primary.r,
-            		$scope.config.tools.colors.primary.g,
-            		$scope.config.tools.colors.primary.b)
-
-	$scope.color = {
-        H: hsv.H,
-        S: hsv.S,
-        V: hsv.V,
-        hex: "#000000",
+    $scope.palette = {
+        color: true,
+        constants: true,
     }
 
     var palmousedown = false;
-	var huemousedown = false;
+    var huemousedown = false;
 
-    var palette = document.getElementById("palette");
-    var hue = document.getElementById("hue");
-
-    var palContext = palette.getContext('2d');
-    var hueContext = hue.getContext('2d');
-
-    var palBox = palette.getBoundingClientRect();
-    var hueBox = hue.getBoundingClientRect();
+    $scope.palette.constants = {
+        palmousedown: false,
+        huemousedown: false,
+        pal: document.getElementById("palette"),
+        hue: document.getElementById("hue"),
+        palContext: palette.getContext('2d'),
+        hueContext: hue.getContext('2d'),
+        palBox: palette.getBoundingClientRect(),
+        hueBox: hue.getBoundingClientRect(),
+    }
 
     var palImg = new Image();
     palImg.src = "assets/img/bgGradient.png";
@@ -41,36 +37,54 @@ app.controller('PaletteCtrl', function($scope) {
     hueImg.src = "assets/img/hueBar.png";
 
     hueImg.onload = function() {
-        $scope.$watch('color', function(newVal, oldVal) {
+        $scope.$watch('palette.color', function(newVal, oldVal) {
+            if(newVal == true) {
+                return;
+            }
             rgb = HSVtoRGB(newVal.H, newVal.S, newVal.V);
             $scope.config.tools.colors.primary.r = rgb.R;
             $scope.config.tools.colors.primary.g = rgb.G;
             $scope.config.tools.colors.primary.b = rgb.B;
-            $scope.color.hex = RGBtoHEX(rgb.R, rgb.G, rgb.B);
+            $scope.palette.color.hex = RGBtoHEX(rgb.R, rgb.G, rgb.B);
 
-            drawMarker(palette, palBox, palContext, palImg);
-            drawBar(hue, hueBox, hueContext, hueImg);
+            drawMarker(palImg);
+            drawBar(hueImg);
         }, true);
     };
 
-	/* init */
-	$scope.init = function() {
-		$scope.setCursor('default');
-	};
+    /* init */
+    $scope.init = function() {
+        $scope.setCursor('default');
 
-	$scope.mouseDown = function() {
+        hsv = RGBtoHSV( $scope.config.tools.colors.primary.r,
+                        $scope.config.tools.colors.primary.g,
+                        $scope.config.tools.colors.primary.b);
 
-	};
+        hexcode = RGBtoHEX( $scope.config.tools.colors.primary.r,
+                            $scope.config.tools.colors.primary.g,
+                            $scope.config.tools.colors.primary.b);
 
-	/* onMouseUp */
-	$scope.mouseUp = function() {
+        $scope.palette.color = {
+            H: hsv.H,
+            S: hsv.S,
+            V: hsv.V,
+            hex: hexcode,
+        }
+    };
 
-	};
+    $scope.mouseDown = function() {
 
-	/* onMouseMove */
-	$scope.mouseMove = function() {
-		
-	};
+    };
+
+    /* onMouseUp */
+    $scope.mouseUp = function() {
+
+    };
+
+    /* onMouseMove */
+    $scope.mouseMove = function() {
+        
+    };
 
     $scope.palMouseDown = function(event) {
         updateSV(event);
@@ -102,65 +116,79 @@ app.controller('PaletteCtrl', function($scope) {
         huemousedown = false;
     }
 
-	/*
-	 * This will watch for this tools' "active" variable changes.
-	 * When "active" changes to "true", this tools functions need to
-	 * be registered to the global config.
-	 * This functions NEEDS to be in each tools controller for
-	 * the tool to function. Please assign the correct toolfunctions
-	 * to the "activeToolFunctions" object.
-	 * Always call "init" first;
-	 */
-	$scope.$watch('active', function(nval, oval) {
-		if (nval) {
-			$scope.init();
+    /*
+     * This will watch for this tools' "active" variable changes.
+     * When "active" changes to "true", this tools functions need to
+     * be registered to the global config.
+     * This functions NEEDS to be in each tools controller for
+     * the tool to function. Please assign the correct toolfunctions
+     * to the "activeToolFunctions" object.
+     * Always call "init" first;
+     */
+    $scope.$watch('active', function(nval, oval) {
+        if (nval) {
+            $scope.init();
 
-			$scope.config.tools.activeToolFunctions = {
-				mouseDown: $scope.mouseDown,
-				mouseUp: $scope.mouseUp,
-				mouseMove: $scope.mouseMove
-			};
-		}
-	}, true);
+            $scope.config.tools.activeToolFunctions = {
+                mouseDown: $scope.mouseDown,
+                mouseUp: $scope.mouseUp,
+                mouseMove: $scope.mouseMove
+            };
+        }
+    }, true);
 
-	function updateSV(event) {
-        $scope.color.S = Math.floor((event.offsetX*100)/(palBox.right - palBox.left));
-        $scope.color.V = Math.floor(100-(event.offsetY*100)/(palBox.bottom - palBox.top));
+    function updateSV(event) {
+        $scope.palette.color.S = Math.floor(
+            ((event.clientX - $scope.palette.constants.palBox.left)*100)/
+            ($scope.palette.constants.palBox.right - $scope.palette.constants.palBox.left)
+        );
+        $scope.palette.color.V = Math.floor(
+            100-((event.clientY - $scope.palette.constants.palBox.top)*100)/
+            ($scope.palette.constants.palBox.bottom - $scope.palette.constants.palBox.top)
+        );
     }
 
     function updateH(event) {
-        $scope.color.H = Math.floor(360-(event.offsetY*360)/(hueBox.bottom - hueBox.top));
+        $scope.palette.color.H = Math.floor(
+            360-((event.clientY - $scope.palette.constants.hueBox.top)*360)/
+            ($scope.palette.constants.hueBox.bottom - $scope.palette.constants.hueBox.top)
+        );
     }
 
-    function drawMarker(palette, palBox, palContext, palImg) {
-        palContext.clearRect(0, 0, palette.width, palette.height);
-        palContext.drawImage(palImg, 0, 0);
-        palContext.beginPath();
-        locX = $scope.color.S * (palBox.right - palBox.left)/100;
-        locY = (100 - $scope.color.V) * (palBox.bottom - palBox.top)/100;
-        palContext.arc(locX,locY,5,0,2*Math.PI, false);
-        palContext.fillStyle = "rgb("  + $scope.config.tools.colors.primary.r + "," 
+    function drawMarker(palImg) {
+        context = $scope.palette.constants.palContext;
+        context.clearRect(0, 0, $scope.palette.constants.pal.width, $scope.palette.constants.pal.height);
+        context.drawImage(palImg, 0, 0);
+        context.beginPath();
+        locX =  $scope.palette.color.S * 
+                ($scope.palette.constants.palBox.right - $scope.palette.constants.palBox.left)/100;
+        locY =  (100 - $scope.palette.color.V) * 
+                ($scope.palette.constants.palBox.bottom - $scope.palette.constants.palBox.top)/100;
+        context.arc(locX,locY,5,0,2*Math.PI, false);
+        context.fillStyle = "rgb("  + $scope.config.tools.colors.primary.r + "," 
                                     + $scope.config.tools.colors.primary.g + "," 
                                     + $scope.config.tools.colors.primary.b + ")";
-        palContext.fill();
-        palContext.lineWidth = 2;
-        palContext.strokeStyle = "black";
-        palContext.stroke();
-        palContext.lineWidth = 1;
-        palContext.strokeStyle = "white";
-        palContext.stroke();
+        context.fill();
+        context.lineWidth = 2;
+        context.strokeStyle = "black";
+        context.stroke();
+        context.lineWidth = 1;
+        context.strokeStyle = "white";
+        context.stroke();
     }
 
-    function drawBar(hue, hueBox, hueContext, hueImg) {
-        hueContext.clearRect(0, 0, hue.width, hue.height);
-        hueContext.drawImage(hueImg, 0, 0, hue.width, hue.height);
-        hueContext.beginPath();
-        loc = (360 - $scope.color.H) * (hueBox.bottom - hueBox.top)/360;
-        hueContext.rect(0, loc - 3, hueBox.right-hueBox.left, 6);
-        hueContext.fillStyle = "hsl(" + $scope.color.H + ", 100%, 50%)";
-        hueContext.fill();
-        hueContext.lineWidth = 2;
-        hueContext.strokeStyle = "black";
-        hueContext.stroke();
+    function drawBar(hueImg) {
+        context = $scope.palette.constants.hueContext;
+        context.clearRect(0, 0, $scope.palette.constants.hue.width, $scope.palette.constants.hue.height);
+        context.drawImage(hueImg, 0, 0, $scope.palette.constants.hue.width, $scope.palette.constants.hue.height);
+        context.beginPath();
+        loc =   (360 - $scope.palette.color.H) * 
+                ($scope.palette.constants.hueBox.bottom - $scope.palette.constants.hueBox.top)/360;
+        context.rect(0, loc - 3, $scope.palette.constants.hueBox.right-$scope.palette.constants.hueBox.left, 6);
+        context.fillStyle = "hsl(" + $scope.palette.color.H + ", 100%, 50%)";
+        context.fill();
+        context.lineWidth = 2;
+        context.strokeStyle = "black";
+        context.stroke();
     }
 });
