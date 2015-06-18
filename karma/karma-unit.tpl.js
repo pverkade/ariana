b86759d9d033e2373f7f0996858c552a529513cc
Jurre Wolsink <jurre.wolsink@gmail.com>
2015-06-18 17:28:29 +0200

    tests for AppCtrl

--- karma/karma-unit.tpl.js
+++ karma/karma-unit.tpl.js
@@ -49,6 +49,6 @@ module.exports = function(karma) {
          * open and the tests will automatically occur there during the build. This has
          * the aesthetic advantage of not launching a browser every time you save.
          */
-        browsers: ['PhantomJS'],
+        browsers: ['Firefox'],
     });
 };
