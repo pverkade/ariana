583cf9a41d10e14e3a4a49782b8cbb5969718166
David Veenstra <davidjulianveenstra@gmail.com>
2015-06-21 21:36:34 +0200

    fixed saturation slider not working

--- src/shaders/saturation.frag.glsl
+++ src/shaders/saturation.frag.glsl
@@ -2,7 +2,7 @@ precision mediump float;
 
 const vec3 gMonoMult = vec3(0.299, 0.587, 0.114);
 
-uniform float u_saturationValue;
+uniform float u_intensity;
 uniform sampler2D u_texture;
 varying vec2 v_texCoord;
 
@@ -10,6 +10,6 @@ void main() {
 	vec4 texColor = texture2D(u_texture, v_texCoord);
 	vec3 result = vec3(dot(texColor.xyz, gMonoMult));
 
-	result = mix(result, texColor.xyz, u_saturationValue);
+	result = mix(result, texColor.xyz, u_intensity);
 	gl_FragColor = vec4(result, texColor.a);
 }
