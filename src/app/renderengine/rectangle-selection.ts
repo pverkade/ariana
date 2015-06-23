c3831ef455d96147d6d1193a70deb27be38e3f44
Merwin van Dijk <merwinvandijk@gmail.com>
2015-06-23 22:09:51 +0200

    rectangle selection fixed bug

--- src/app/renderengine/rectangle-selection.ts
+++ src/app/renderengine/rectangle-selection.ts
@@ -1,6 +1,11 @@
 class Rectangle{
 	point1 : Point;
 	point2 : Point;
+
+	constructor(point1 : Point, point2 : Point) {
+		this.point1 = new Point(point1.x, point1.y);
+		this.point2 = new Point(point2.x, point2.y);
+	}
 }
 
 
@@ -19,6 +24,8 @@ class RectangleSelection extends AbstractSelection implements SelectionInterface
 		var width =  bottomRight.x - topLeft.x;
 		var height = bottomRight.y - topLeft.y;
 		var indexPointMask : number;
+
+		this.rects.push(new Rectangle(point1, point2));
 
 		/* Draw borders in mask. */
 		for (var y = 0; y < height; y++) {
@@ -53,11 +60,42 @@ class RectangleSelection extends AbstractSelection implements SelectionInterface
 
 	clearLast() : boolean {
 		var nrWands = this.maskWandParts.length;
+		var point1 = this.rects[this.rects.length - 1].point1;
+		var point2 = this.rects[this.rects.length - 1].point2;
+
+		var topLeft = new Point(Math.min(point1.x, point2.x), Math.min(point1.y, point2.y));
+		var bottomRight = new Point(Math.max(point1.x, point2.x), Math.max(point1.y, point2.y));
+
+		var width =  bottomRight.x - topLeft.x;
+		var height = bottomRight.y - topLeft.y;
 		var indexPointMask;
 
 		if (nrWands < 1) {
 			return false;
 		}
+
+		for (var y = 0; y < height; y++) {
+			indexPointMask = (topLeft.y + y) * this.width + topLeft.x;
+			this.maskBorder[indexPointMask] = 0;
+			indexPointMask = (topLeft.y + y) * this.width + bottomRight.x;
+			this.maskBorder[indexPointMask] = 0;
+		}
+
+		for (var x = 0; x < width; x++) {
+			indexPointMask = topLeft.y * this.width + topLeft.x + x;
+			this.maskBorder[indexPointMask] = 0;
+			indexPointMask = bottomRight.y * this.width + topLeft.x + x;
+			this.maskBorder[indexPointMask] = 0;
+		}
+
+		for (var i = 0; i < this.maskWandParts[nrWands - 1].length; i++) {
+			if (this.maskWandParts[nrWands - 1][i] == 1) {
+				this.maskWand[i] = 0;
+				this.maskWandParts[nrWands - 1][i] = 0;
+			}
+		}
+
+		this.maskWandParts.splice(nrWands - 1, 1);
 
 		return true;
 	}
