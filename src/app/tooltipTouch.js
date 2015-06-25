f58836a8252b8513c0338eafd83df0e868ded026
Scoudem <tvvaalen@gmail.com>
2015-06-25 19:42:42 +0200

    Tooltip touch fixed?

--- src/app/tooltipTouch.js
+++ src/app/tooltipTouch.js
@@ -1,24 +1,16 @@
-app.config(['$tooltipProvider',
-    function ($tooltipProvider) {
-        var tooltipFactory = $tooltipProvider.$get[$tooltipProvider.$get.length - 1];
+// configure the tooltipProvider
+app.config(['$tooltipProvider', function ($tooltipProvider) {
 
-        // decorate the tooltip getter
-        this.$get = [ '$window', '$compile', '$timeout', '$parse', '$document', '$position', '$interpolate',
-            function ( $window, $compile, $timeout, $parse, $document, $position, $interpolate ) {
-                // for touch devices, don't return tooltips
-                if ('ontouchstart' in $window) {
-                    console.log('if');
-                    return function () {
-                        return {
-                            compile: function () { }
-                        };
-                    };
-                } else {
-                    console.log($interpolate);
-                    // run the default behavior
-                    return tooltipFactory($window, $compile, $timeout, $parse, $document, $position, $interpolate);
-                }
-            }
-        ];
+    var parser = new UAParser();
+    var result = parser.getResult();
+    var touch = result.device && (result.device.type === 'tablet' || result.device.type === 'mobile');
+
+    if (touch) {
+        var options = {
+            trigger: 'dontTrigger' // default dummy trigger event to show tooltips
+        };
+
+        $tooltipProvider.options(options);
     }
-]);
+
+}]);
